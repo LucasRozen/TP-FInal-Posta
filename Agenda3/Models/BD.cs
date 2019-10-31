6e27730c1b20ad91717cecfe30239659534dd802
GONZALO NAHUEL SOSIN <44713226@comunidadort.edu.ar>
2019-10-31 09:56:14 -0300

    Agregar archivos de proyecto.

---
+++ Agenda3/Models/BD.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Agenda3.Models
+{
+    public class BD
+    {
+        public static string connectionString = "Server=.;Database=Agenda3;Trusted_Connection=True;";
+        private static SqlConnection Conectar()
+        {
+            SqlConnection Conn = new SqlConnection(connectionString);
+            Conn.Open();
+            return Conn;
+        }
+        public static void desconectar(SqlConnection Conexion)
+        {
+            Conexion.Close();
+        }
+        
+
+        public static List<TiposEve> ListarNoticias()
+        {
+
+            List<TiposEve> Lista = new List<TiposEve>();
+            SqlConnection Conexion = Conectar();
+            SqlCommand Consulta = Conexion.CreateCommand();
+            Consulta.CommandText = "SELECT * From TiposDeEventos";
+            Consulta.CommandType = System.Data.CommandType.Text;
+            SqlDataReader dataReader = Consulta.ExecuteReader();
+            while (dataReader.Read())
+            {
+                TiposEve Eventos = new TiposEve();
+                Eventos.idTipEve = Convert.ToInt32(dataReader["idTipoEve"]);
+                EVentos.TipEve = dataReader["NombreT"].ToString();
+                
+                Lista.Add(Eventos);
+            }
+            desconectar(Conexion);
+            return Lista;
+
+        }
+    }
+}
---
+++ Agenda3/Models/TiposEve.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenda3.Models
+{
+    public class TiposEve
+    {
+
+        private int _idTipEve;
+        private string _TipEve;
+
+        public int IdTipEve { get => _idTipEve; set => _idTipEve = value; }
+        public string TipEve { get => _TipEve; set => _TipEve = value; }
+
+
+        public TiposEve()
+        {
+
+        }
+
+        public TiposEve(int idTipEve, string TipEve)
+        {
+            _idTipEve = idTipEve;
+            _TipEve = TipEve;
+        }
+
+        
+    }
+
+    
+
+}
