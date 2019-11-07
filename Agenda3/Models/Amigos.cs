5821fed4db635604203777eb09f83ef13a80be58
GONZALO NAHUEL SOSIN <44362491@comunidadort.edu.ar>
2019-11-07 09:47:49 -0300

    a

--- Agenda3/Models/Amigos.cs
+++ Agenda3/Models/Amigos.cs
@@ -25,3 +25,4 @@ namespace Agenda3.Models
         }
 
     }
+}
--- Agenda3/Models/Evento.cs
+++ Agenda3/Models/Evento.cs
@@ -11,18 +11,25 @@ namespace Agenda3.Models
         private string _Nombre;
         private int _idTipEve;
         private int _idAmigo;
-        private DateTime Dia;
-        private string Descipcion;
-        private bool Activo;
-        private bool Destac;
+        private DateTime _Dia;
+        private string _Descipcion;
+        private bool _Activo;
+        private bool _Destac;
         public int IdEve { get => _IdEve; set => _IdEve = value; }
         public string Nombre { get => _Nombre; set => _Nombre = value; }
         public int IdTipEve { get => _idTipEve; set => _idTipEve = value; }
         public int IdAmigo { get => _idAmigo; set => _idAmigo = value; }
-        public DateTime Dia { get => Dia; set => Dia = value; }
-        public string Descipcion { get => Descipcion; set => Descipcion = value; }
-        public bool Activo { get => Activo; set => Activo = value; }
-        public bool Destac { get => Destac; set => Destac = value; }
+        public DateTime Dia { get => _Dia; set => _Dia = value; }
+        public string Descipcion { get => _Descipcion; set => _Descipcion = value; }
+        public bool Activo { get => _Activo; set => _Activo = value; }
+        public bool Destac { get => _Destac; set => _Destac = value; }
+
+
+        public Evento()
+        {
+
+        }
+
 
         public Evento(int IdEve, string nom,int IdTEve, int IdAmi, DateTime dia, string descr, bool Act, bool destac)
         {
@@ -37,10 +44,7 @@ namespace Agenda3.Models
         }
            
 
-        public Evento()
-        {
-
-        }
+       
 
         
     }
