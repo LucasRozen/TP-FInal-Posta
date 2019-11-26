972de90d6283300879bff47040ba6a3e0d0266f8
LUCAS LUIS ROZENBERG <44452015@comunidadort.edu.ar>
2019-11-26 15:38:04 -0300

    stfh

--- Agenda3/Models/BD.cs
+++ Agenda3/Models/BD.cs
@@ -161,9 +161,9 @@ namespace Agenda3.Models
             Consulta.CommandText = "sp_InsertarAmigo";
 
             Consulta.Parameters.AddWithValue("@NomA", nombre);
-//             Consulta.Parameters.AddWithValue("@NomA", nombre);
-//            Consulta.Parameters.AddWithValue("@NomA", nombre);
-//            Consulta.Parameters.AddWithValue("@NomA", nombre);
+//          Consulta.Parameters.AddWithValue("@NomA", nombre);
+//          Consulta.Parameters.AddWithValue("@NomA", nombre);
+//          Consulta.Parameters.AddWithValue("@NomA", nombre);
 
             Consulta.ExecuteNonQuery();
             
